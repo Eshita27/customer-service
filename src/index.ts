2086ce314eeea7b404cf4bc6306446adb50fe928
Eshita Mukherjee <eshitachakraborty@outlook.com>
2025-11-17 12:27:45 +0530

    feat: integrate Swagger UI and generate OpenAPI documentation for customer service

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import customerRoutes from './routes/customerRoutes';
+import swaggerUi from 'swagger-ui-express';
+import { openApiDocument } from './config/openapi';
 
 dotenv.config();
 const app = express();
@@ -13,3 +15,5 @@ app.use('/api/v1/customers', customerRoutes);
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocument));
